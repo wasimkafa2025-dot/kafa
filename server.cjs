7480aa330c69aa1d53cc3997e4fcfd0a0de07596
wasimkafa2025-dot <wasimkafa2025@gmail.com>
2026-09-11 09:30:44 +0000

    Deploying to gh-pages from @ wasimkafa2025-dot/kafa@428a9c5f23ce8eb34d7c01909fcde2ea211dbb49 🚀

--- server.cjs
+++ server.cjs
@@ -340,7 +340,7 @@ async function startServer() {
       res.status(500).json({ success: false, error: err?.message || "Manual check failed" });
     }
   });
-  const DEFAULT_SHEETS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyx__UhJ9vOS0s_WvSlhRLgS6FxKPJEHPnBZMqm4I2dzaeQGayV7FIBTbIvpOlbLu-8yg/exec";
+  const DEFAULT_SHEETS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw7ZftkEvhPqDKRfpxSPcl8lAq-BdkIdPGvx8yVe7FwXgNwbMnfswQlOnK4o_1xlxykgg/exec";
   app.post("/api/sheets/sync", async (req, res) => {
     try {
       const { url, payload } = req.body;
